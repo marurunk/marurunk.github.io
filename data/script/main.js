b2a8fc9b2b2306d3466001fea97238062d6fd005
marurunk <kunmarun18@gmail.com>
2023-11-23 15:43:17 -0500

    gallery, home - beta

--- data/script/main.js
+++ data/script/main.js
@@ -1,46 +1,152 @@
+const ELEMENT = [
+    'Resilience',
+    'Passion',
+    'Hate',
+    'Ambition',
+]
+
+const INFORMATION = [
+    [
+        'Maru',
+        'Lorem error provident velit iure adipisci. Sed praesentium similique repellat impedit quos vitae consectetur mollitia Veniam alias at consectetur consectetur elit odio? Alias vero sunt in fuga sapiente, vel! Ipsa quibusdam quam architecto eligendi obcaecati Sunt dolorem id hic magni dolore! Quis accusamus optio tempora quia veniam! Unde tenetur animi.',
+    ],
+    [
+        'Teku',
+        'Lorem error provident velit iure adipisci. Sed praesentium similique repellat impedit quos vitae consectetur mollitia Veniam alias at consectetur consectetur elit odio? Alias vero sunt in fuga sapiente, vel! Ipsa quibusdam quam architecto eligendi obcaecati Sunt dolorem id hic magni dolore! Quis accusamus optio tempora quia veniam! Unde tenetur animi.',
+    ],
+    [
+        'Yuki',
+        'Lorem error provident velit iure adipisci. Sed praesentium similique repellat impedit quos vitae consectetur mollitia Veniam alias at consectetur consectetur elit odio? Alias vero sunt in fuga sapiente, vel! Ipsa quibusdam quam architecto eligendi obcaecati Sunt dolorem id hic magni dolore! Quis accusamus optio tempora quia veniam! Unde tenetur animi.',
+    ],
+    [
+        'Nogi',
+        'Lorem error provident velit iure adipisci. Sed praesentium similique repellat impedit quos vitae consectetur mollitia Veniam alias at consectetur consectetur elit odio? Alias vero sunt in fuga sapiente, vel! Ipsa quibusdam quam architecto eligendi obcaecati Sunt dolorem id hic magni dolore! Quis accusamus optio tempora quia veniam! Unde tenetur animi.',
+    ],
+]
+
+
+const themeProperties = [
+    '--theme-accent',
+    '--theme-bg-color',
+]
+const themeValues = [
+    [
+        '#0bf',
+        '#035',
+    ],
+
+    [
+        '#f05',
+        '#503',
+    ],
+
+    [
+        '#f30',
+        '#533',
+    ],
+
+    [
+        '#fd0',
+        '#543',
+    ],
+
+]
+
+const total_images = 4;
+const titles = [
+    "Maru 2023",
+    "Teku 2023",
+    "Yuki 2023",
+    "Nogi 2023",
+]
+
+const trelloLink = "https://trello.com/b/2nSioGdD/marunk-work.json";
+
 const button_list = [
     document.getElementById('mobile-instagram'),
     document.getElementById('mobile-twitter'),
-    document.getElementById('mobile-pixiv'),/* 
-    document.getElementById('Telegram') */
+    document.getElementById('mobile-pixiv'),
 ]
 let effect3 = new Audio('./data/audio/wave-in.wav');
 let effect5 = new Audio('./data/audio/wave-out.wav');
 
 let principal_character = document.getElementById('social-media-character');principal_character.oncontextmenu = ()=>{return false};
 
-const trelloLink = "https://trello.com/b/2nSioGdD/marunk-work.json"
+let gallery_box = document.getElementById('gallery');
+
 
 function init() {
+
+    genGallery();
     initSound();
     initButtons();
-    
     detectDevice();
+    genRandomTheme(ELEMENT.length);
 
-    // Animation Fade-In
-    let animation_fadeInit = document.getElementById('init').animate(
+}
+
+function genRandomTheme(total_themes) {
+
+    let x = Math.random() * total_themes;
+    x = Math.round(x)
+    if ( x == 0 ) { x=x+1; }
+    let loader= document.getElementById('loader')
+    loader.innerHTML = `
+    <span class="flex box">${ELEMENT[x-1]}</span>
+    `;
+
+
+    // SET BACKGROUND THEME
+    let bg = document.getElementById('background-illustration').children[0];
+    bg.onload = function () {
+        let animation_fadeInit = loader.animate(
         [
-          { opacity: '0' },
-          { opacity: '1' }
+          { opacity: '1' },
+          { opacity: '1' },
+          { opacity: '0.7'},
+          { opacity: '0' , backgroundColor: `${themeValues[x-1][0]}`, visibility: 'hidden'}
         ], {
           fill: 'forwards',
           duration: 1500
         });
-    
+        animation_fadeInit.play();
 
-    animation_fadeInit.play();
-    
+       let animation2_fadeInit = loader.children[0].animate(
+        [
+          { color: "#000000aa", textShadow: "0 0 0px #000000"},
+          { color: "#00000000", textShadow: "0 0 10px #000000"},
+          { color: "#00000000", textShadow: "0 0 30px #000000"},
+          { color: "#00000000", textShadow: "0 0 60px #000000"}
+        ], {
+          fill: 'forwards',
+          duration: 1000
+        });
+        animation2_fadeInit.play();
+
+    };
+    bg.src = `data/img/bg${x}.jpg`;
+
+    // SET CSS VARIABLES THEME
+
+    let root = document.querySelector(':root');
+    console.log ( themeProperties[0] + "  :  " + themeValues[x-1][0]);
+    for (let i=0; i<themeProperties.length; ++i) {
+        root.style.setProperty(themeProperties[i], themeValues[x-1][i]);
+    }
+
+    // SET CHARACTER THEME
+
+
 }
 
 function initSound() {
     effect3.volume = 1;
     effect5.volume = 1;
-    
+
     var playPromise = effect5.play();
     //Validated if sounds was played
     if (playPromise !== undefined) {
         playPromise.then(_ => {
-            console.log("sound started! :)")
 
         })
         .catch(error => {
@@ -55,8 +161,19 @@ function initButtons() {
             effect3.play()
         })
     // button.addEventListener( 'pointerenter', ()=>{});
-    })    
+    })
 }
+
+function genGallery() {
+     for (let i = 1;  i <= total_images ; i++) {
+        gallery_box.innerHTML += `
+        <div class="gallery-image">
+            <a class="gallery-image" data-lightbox="portfolio" data-title="${titles[i-1]}" href="data/img/slides/${i}.png"><img src="data/img/slides/small/${i}.jpg" alt=""></a>
+        </div>
+        `;
+    }
+}
+
 
 function detectDevice(){
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
@@ -68,11 +185,11 @@ function detectDevice(){
             console.log(`Your system is: MacOS`)
         } else if (/Linux/.test(navigator.userAgent)) {
             console.log(`Your system is: Linux`)
-        
+
         } else {
             console.log(`Your platform is hidden.. \n Nice`)
         }
-      
+
     }
 }
 
@@ -90,7 +207,7 @@ tabs.forEach((tab, index)=>{
     tab.addEventListener('click', (e)=>{
         tabs.forEach(tab=>{tab.classList.remove('tab-active')});
         tab.classList.add('tab-active')
-    
+
         var line = document.querySelector('.tabs-line');
         line.style.width = e.target.offsetWidth + "px";
         line.style.left = e.target.offsetLeft + "px";
@@ -117,12 +234,12 @@ fetch(trelloLink)
 
     // POR CADA LISTA DE LA TABLA
     data.lists.forEach(function(list){
-        
+
         html += `
             <div class="flex col kanban-list" id="${list.name.replace(/\s/g, "")}">
             <h3 class="kanban-list-title">${list.name}</h3>
         `// INICIAMOS LA LISTA
-        
+
         data.cards.forEach(function(card){
             // FILTRAMOS LAS CARTAS CORRESPONDIENTES A LA LISTA USANDO EL ID DE LA LISTA
             if (card.idList == list.id){
@@ -131,7 +248,6 @@ fetch(trelloLink)
                 card.labels.forEach(function(label){
                     labelsHtml += `<span class="kanban-label ${label.color}">` +label.name + `</span>
                     `;
-                    console.log(label.color + " | " + label.name)
                 });
                 //AGREGAMOS CADA CARTA EN EL HTML CON SUS ETIQUETAS
                 html += `
